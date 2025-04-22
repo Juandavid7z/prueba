41ea4bc05b2a5b30a51188703c6b67c116ff788f
JuanDavid7z <juandavid09022005@gmail.com>
2025-04-22 10:50:53 -0500

    chore: se configura swagger

---
+++ swagger/swagger.ts
@@ -0,0 +1,25 @@
+import swaggerJsdoc from 'swagger-jsdoc';
+
+const options = {
+    definition: {
+        openapi: '3.0.0',
+        info: {
+            title: 'Task API',
+            version: '1.0.0',
+            description: 'API for managing Task',
+            contact: {
+                name: 'Juan Munoz'
+            },
+            servers: [
+                {
+                    url: 'http://localhost:3001',
+                    description: 'Local server'
+                }
+            ]
+        }
+    },
+    apis: ['./src/routes/*.ts']
+};
+
+const specs = swaggerJsdoc(options);
+export default specs;
