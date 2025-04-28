caf60a8a9a0e13ae0893f81c7009138ec1472a18
JuanDavid7z <juandavid09022005@gmail.com>
2025-04-28 14:43:02 -0500

    feat: se agrega servicio para la gestión de tareas

---
+++ src/services/TaskServices.ts
@@ -0,0 +1,21 @@
+import { Task } from "../models/tasks";
+import { TaskRepository } from "../repositories/TaskRepository";
+
+export const TaskService = {
+  getAll: () => TaskRepository.findAll(),
+
+  getById: (id: number) => TaskRepository.findById(id),
+
+  create: (data: Omit<Task, "id">): Task => {
+    const task: Task = {
+      id: Date.now(), // Id simulado
+      ...data,
+    };
+    return TaskRepository.create(task);
+  },
+
+  update: (id: number, data: Partial<Task>) =>
+    TaskRepository.update(id, data),
+
+  delete: (id: number) => TaskRepository.delete(id),
+};
