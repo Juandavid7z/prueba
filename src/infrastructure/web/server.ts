7c55b972a8413f9c3a6ecfcb3336bbbdf18271e5
JuanDavid7z <juandavid09022005@gmail.com>
2025-04-30 17:32:03 -0500

    chore: implementar servidor Express con rutas y documentación Swagger

---
+++ src/infrastructure/web/server.ts
@@ -0,0 +1,21 @@
+import express, { Request, Response } from 'express';
+import taskRoutes from '../web/routes/tasks';
+import swaggerUI from 'swagger-ui-express';
+import specs from "../../../swagger/swagger";
+import cors from  'cors';
+
+const app = express();
+const port = process.env.PORT || 3001;
+app.use(cors())
+
+app.use(express.json()); // Add this line to enable JSON parsing in the request body
+app.use("/api-docs", swaggerUI.serve, swaggerUI.setup(specs))
+app.use('/api', taskRoutes); // Add this line to mount the Task API routes
+
+app.get('/', (req: Request, res: Response) => {
+  res.send('Hello, TypeScript Express1');
+});
+
+app.listen(port, () => {
+  console.log(`Server running at http://localhost:${port}`);
+});
